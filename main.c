cbd07ad97ff61b9cf578443954a86ed6e24ce24c
[Admir Ljubovic] <[admir.ljubovic@outlook.com]>
2024-07-02 11:22:21 +0200

    Removed commit

--- main.c
+++ main.c
@@ -9,7 +9,6 @@ void print_array(int array[], int length) {
 
 int main(void) {
 
-    // loops
     int length = 10;
 
     int size = length;
@@ -21,21 +20,15 @@ int main(void) {
 
     print_array(myNums, size);
 
-    // pointers and variables
-    // declare an integer
     int num = 5;
     printf("num: %i\n", num);
 
-    // declare a pointer (a variable containing an address to another variable, a memory address)
     int* ptr = &num;
 
-    // declare a value that dereferences the pointer, returning the value it's pointing to
     int val = *ptr;
 
-    // print the value
     printf("dereferenced pointer: %d\n",val);
 
-    // if statements
     int x = 10;
 
     if (x == 10) {
